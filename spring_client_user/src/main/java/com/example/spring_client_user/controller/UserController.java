f360e3e234b2d481dfd2a8ac8f310fef00d5b9b1
Polina Kolesnikova <polina@example.com>
2024-04-30 02:07:39 +0200

    adding repository

--- spring_client_user/src/main/java/com/example/spring_client_user/controller/UserController.java
+++ spring_client_user/src/main/java/com/example/spring_client_user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.spring_client_user.controller;
 
+import com.example.spring_client_user.model.User;
 import com.example.spring_client_user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -14,7 +17,7 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/users")
-    public  String getAllUsers(){
-        return service.getAllUsers().toString();
+    public List<User> getAllUsers(){
+        return service.getAllUsers();
     }
 }
--- spring_client_user/src/main/java/com/example/spring_client_user/repository/UserRepository.java
+++ spring_client_user/src/main/java/com/example/spring_client_user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.spring_client_user.repository;
 
 import com.example.spring_client_user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer > {
 }
